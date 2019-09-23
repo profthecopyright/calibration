5ce9534af175c058d7baa746f37817eb013f4e62
fuckthecopyright <54515051+fuckthecopyright@users.noreply.github.com>
2019-09-23 15:36:53 -0400

    add center screen function and randomized textures

--- cali_init.m
+++ cali_init.m
@@ -1,12 +1,19 @@
-function cali_init(hwin, hgridfunc, pattern, multiplier)
+function cali_init(hwin, hgridfunc, pattern, multiplier, align_mode)
 global cali
     cali.grids =            hgridfunc();
     cali.image_height =     160;
     cali.image_width =      160;
-    cali.image_lib_size =   10;  % size of image library
+    cali.image_lib_size =   21;  % size of image library
     cali.total_face_num =   cali.image_lib_size - 1;     % the last for white square, not face
     cali.image_lib =        cell(1, cali.image_lib_size);
     cali.multiplier =       multiplier;
+    
+    switch align_mode
+        case 'center'
+            cali.grid_offset = [0.5; 0];
+        otherwise
+            cali.grid_offset = [0; 0];
+    end
     
     for i = 1 : cali.image_lib_size
         cali.image_lib{i} = imread(['D:\GitHub\EyeTrackerCalibration\images\m' num2str(i) '.png']);
@@ -20,20 +27,33 @@ global cali
     for i = 1 : cali.image_lib_size
         cali.tex_lib(i) = Screen('MakeTexture', cali.stimulus_window, cali.image_lib{i});
     end
+    
+    cali.tex_seq = cell(length(cali.grids), 1);
+    
     switch pattern
+        
         case 'face'
-            cali.tex_map = randi(cali.total_face_num, 16, 9); 
+            for i = 1 : length(cali.grids)
+                cali.tex_seq{i} = randi(cali.total_face_num, 1, size(cali.grids{i}, 2));
+            end
+            
+            %cali.tex_map = randi(cali.total_face_num, 16, 9); 
             %[x y], matching the order from grids
         case 'dot'
-            cali.tex_map = ones(16, 9) * cali.image_lib_size; 
+            for i = 1 : length(cali.grids)
+                cali.tex_seq{i} = ones(1, size(cali.grids{i}, 2)) * cali.image_lib_size;
+            end
+            % cali.tex_map = ones(16, 9) * cali.image_lib_size; 
             % for white dots
         otherwise
             disp('pattern not recognized')
     end
+    
     currentdatestr = datestr(now, 30);
     currentdatestr = currentdatestr(3:end);
     grids = cali.grids;
-    tex_map = cali.tex_map;
-    save(['D:\GitHub\EyeTrackerCalibration\' currentdatestr '.mat'], 'grids', 'tex_map');
+    tex_seq = cali.tex_seq;
+    %tex_map = cali.tex_map;
+    save(['D:\GitHub\EyeTrackerCalibration\' currentdatestr '.mat'], 'grids', 'tex_seq');
 
 end
--- cali_run.m
+++ cali_run.m
@@ -6,24 +6,13 @@ global cali
     end
 
     grid = cali.grids{trial_id};
-    num = size(grid, 2);
-    
-    if num == 1
-        ind = cali.tex_map(grid(1), grid(2)); % [x y]
-        tex_list = cali.tex_lib(ind);
-    else
-        tex_list = zeros(1, num);
-
-        for j = 1 : num
-            ind = cali.tex_map(grid(1, j), grid(2, j)); % [x y]
-            tex_list(j) = cali.tex_lib(ind);
-        end
-    end
+    tex_ind = cali.tex_seq{trial_id};
+    tex_list = cali.tex_lib(tex_ind);
     
     margin = (1 - cali.multiplier) / 2;
     
-    left_top_corners = (grid - 1 + margin) .* ([cali.image_width; cali.image_height] * ones(1, num));
-    right_bottom_corners = (grid - margin) .* ([cali.image_width; cali.image_height] * ones(1, num));
+    left_top_corners = (grid - 1 + margin + cali.grid_offset) .* ([cali.image_width; cali.image_height] * ones(1, num));
+    right_bottom_corners = (grid - margin + cali.grid_offset) .* ([cali.image_width; cali.image_height] * ones(1, num));
     rectangles = [left_top_corners; right_bottom_corners];
     % rotations = zeros(1, num);
 
